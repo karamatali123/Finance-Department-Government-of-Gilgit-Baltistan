55264e9d08caae838b7886ef7a59c2f987779648
salman <click8salman@gmail.com>
2024-12-23 14:31:21 +0500

    ui fix

--- app/Components/HomePage/FeatureCards.js
+++ app/Components/HomePage/FeatureCards.js
@@ -18,8 +18,8 @@ const cards = [
 
 const CardSection = () => {
   return (
-    <section className="py-16 bg-white">
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <section className="py-8 md:py-16 bg-white">
+      <div className="container mx-auto px-4 md:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
         {cards.map((card, index) => (
           <div
             key={index}
