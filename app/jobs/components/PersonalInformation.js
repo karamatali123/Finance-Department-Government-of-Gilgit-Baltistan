d8d68156f0089c62609bd4293042b6f142bf4e8d
salman <click8salman@gmail.com>
2025-04-24 00:14:02 +0500

    build fix

--- app/jobs/components/PersonalInformation.js
+++ app/jobs/components/PersonalInformation.js
@@ -76,8 +76,10 @@ const PersonalInformation = ({ handleChange, values }) => {
             required
             className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
           >
-            {dist.map((dist) => (
-              <option value={dist}>{dist}</option>
+            {dist.map((dist, index) => (
+              <option key={index} value={dist}>
+                {dist}
+              </option>
             ))}
           </select>
         </div>
